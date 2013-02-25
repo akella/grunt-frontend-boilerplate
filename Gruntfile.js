797ec5057d18721b70f46003e121e96e87f37f0c
akella <akella.a@gmail.com>
2013-02-25 14:39:08 +0200

    let's go

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+	cssmin: {
+		compress: {
+			files: {
+				"production/css/screen.css": ["production/css/screen.css"]
+			}
+		}
+	},
+	csso: {
+		dist: {
+			src: 'src/css/screen.css',
+			dest:'production/css/screen.min.css'
+		}
+	},
+	compass: {
+		dist: {
+			options: {
+				basePath: 'src/',
+				config: 'src/config.rb',
+				cssDir: 'css',
+				environment: 'production'
+			}
+		}
+	},
+	copy: {
+	  main: {
+		files: [
+			 {expand: true, cwd: 'src/img/', src: ['**'], dest: 'production/img'}, // makes all src relative to cwd
+			 {expand: true, cwd: 'src/sass', src: ['**'], dest: 'production/sass'},
+			 {expand: true, cwd: 'src/css', src: ['**'], dest: 'production/css'},
+			 {expand: true, cwd: 'src/js', src: ['**'], dest: 'production/js'}
+		]
+	  }
+	},
+	includereplace: {
+		dist: {
+			options: {
+				// Global variables available in all files
+				// globals: {
+				//  var1: 'one',
+				//  var2: 'two',
+				//  var3: 'three'
+				// },
+				// Optional variable prefix & suffix, defaults as shown
+				prefix: '@@',
+				suffix: ''
+			},
+			// Files to perform replacements and includes with
+			src: 'src/*.html',
+			// Destinaion directory to copy files to
+			dest: 'production/'
+		}
+	},
+	concat: {
+		options: {
+			separator: ';'
+		},
+		dist: {
+			'src': ['src/js/jquery.js','src/js/jquery.cycle.all.js','src/js/jquery.scrollTo.js','src/js/common.js'],
+			dest: 'production/js/app.js'
+		}
+	},
+	'min': {
+		'dist': {
+			'src': ['src/js/jquery.js','src/js/jquery.cycle.all.js','src/js/jquery.scrollTo.js','src/js/common.js'],
+			'dest': 'production/js/app.js'
+		}
+	},
+	watch: {
+		files: ['src/sass/*.scss', 'src/*html', 'src/js/*.js'],
+		tasks: ['compass', 'copy', 'includereplace', 'concat','csso', 'clean']
+	},
+	clean: ["production/_*.html", "production/css/lib"]
+});
+		grunt.loadNpmTasks('grunt-contrib-compass');
+		grunt.loadNpmTasks('grunt-contrib-cssmin');
+		grunt.loadNpmTasks('grunt-include-replace');
+		grunt.loadNpmTasks('grunt-contrib-copy');
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-clean');
+		grunt.loadNpmTasks('grunt-contrib-concat');
+		grunt.loadNpmTasks('grunt-yui-compressor');
+		grunt.loadNpmTasks('grunt-csso');
+
+
+		grunt.registerTask('default', 'watch');
+
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+My Grunt Boilerplate &copy; akella
+=============
+For generic frontend development. Mostly small to medium projects, and without some heavy JS.
+
+
+`src` - source of the project, where all the development should happen
+
+`production` - code we are sending to client. All preprocessors compiled, css-js-images concatenated and minified.
+
+How to run it
+=============
+ - You need to have _npm_ installed now, as well as a _grunt_.
+ - Download files, and go to this project folder in terminal. Type `npm install` (this will install some libraries for it to work). Folder `node_moduldes` might appear. Don't be afraid.
+ - Run `grunt watch`, and it will watch for all the changes
+
+
+Drop me a line
+=============
+In case it is useful for you, or you have some question, feel free to ask them here [twitter.com/akella](http://twitter.com/akella)
